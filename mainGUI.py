500d236df85ee7bad3123f80a2686778382c2ebf
Mr-RainC0at <changrami@gmail.com>
2022-11-20 23:55:18 +0900

    Commit

--- mainGUI.py
+++ mainGUI.py
@@ -3,12 +3,17 @@ import customtkinter
 from PIL import Image, ImageTk
 import tkintermapview
 import pickle
+
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 fileObj = open('isOpen.obj', 'rb')
 isOpen = pickle.load(fileObj)
 fileObj.close()
+
+fileObj2 = open('data.obj', 'rb')
+allClasses = pickle.load(fileObj2)
+fileObj2.close()
 
 
 class App(customtkinter.CTk):
@@ -25,38 +30,40 @@ class App(customtkinter.CTk):
         self.rowconfigure(0, minsize=50)
         self.columnconfigure(0, weight=1)
 
-        option = 1
+        self.label1 = customtkinter.CTkLabel(self, text="Recommended", text_font=("Noto Sans KR", -20, "bold"))
+        self.label1.grid(row=1, column=0, sticky="w", padx=10)
 
-        if option == 0:
-            self.label1 = customtkinter.CTkLabel(self, text="Recommended", text_font=("Noto Sans KR", -20, "bold"))
-            self.label1.grid(row=1, column=0, sticky="w", padx=10)
+        self.bar1 = VerticalSlider(self)
+        self.bar1.grid(row=2, column=0, sticky="ew")
 
-            self.bar1 = VerticalSlider(self)
-            self.bar1.grid(row=2, column=0, sticky="ew")
+        self.label2 = customtkinter.CTkLabel(self, text="New", text_font=("Noto Sans KR", -20, "bold"))
+        self.label2.grid(row=3, column=0, sticky="w")
 
-            self.label2 = customtkinter.CTkLabel(self, text="New", text_font=("Noto Sans KR", -20, "bold"))
-            self.label2.grid(row=3, column=0, sticky="w")
+        self.bar2 = VerticalSlider(self)
+        self.bar2.grid(row=4, column=0, sticky="ew")
 
-            self.bar2 = VerticalSlider(self)
-            self.bar2.grid(row=4, column=0, sticky="ew")
+        self.label3 = customtkinter.CTkLabel(self, text="Random", text_font=("Noto Sans KR", -20, "bold"))
+        self.label3.grid(row=5, column=0, sticky="w")
 
-            self.label3 = customtkinter.CTkLabel(self, text="Random", text_font=("Noto Sans KR", -20, "bold"))
-            self.label3.grid(row=5, column=0, sticky="w")
+        self.bar3 = VerticalSlider(self)
+        self.bar3.grid(row=6, column=0, sticky="ew")
 
-            self.bar3 = VerticalSlider(self)
-            self.bar3.grid(row=6, column=0, sticky="ew")
 
-        if option == 1:
-            self.rowconfigure(3, weight=1)
+class CultureMapWindow(customtkinter.CTkToplevel):
+    def __init__(self):
+        super().__init__()
+        self.rowconfigure(3, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.geometry("900x700")
 
-            self.label1 = customtkinter.CTkLabel(self, text="컬쳐맵", text_font=("Noto Sans KR", -20, "bold"))
-            self.label1.grid(row=1, column=0, sticky="w")
+        self.label1 = customtkinter.CTkLabel(self, text="컬쳐맵", text_font=("Noto Sans KR", -20, "bold"))
+        self.label1.grid(row=1, column=0, sticky="w")
 
-            self.label1 = customtkinter.CTkLabel(self, text="카테고리", text_font=("Noto Sans KR", -20, "bold"))
-            self.label1.grid(row=2, column=0, sticky="w")
+        self.label1 = customtkinter.CTkLabel(self, text="카테고리", text_font=("Noto Sans KR", -20, "bold"))
+        self.label1.grid(row=2, column=0, sticky="w")
 
-            self.map = CultureMap(self)
-            self.map.grid(row=3, column=0, sticky="nesw")
+        self.map = CultureMap(self)
+        self.map.grid(row=3, column=0, sticky="nesw")
 
 
 class MenuBar(customtkinter.CTkFrame):
@@ -65,7 +72,8 @@ class MenuBar(customtkinter.CTkFrame):
 
         customtkinter.CTkButton(self, text="홈", border_width=2, text_font=("Noto Sans KR", -20, "bold")).place(
             relx=0.01, y=5, relwidth=0.35)
-        customtkinter.CTkButton(self, text="컬쳐맵", border_width=2, text_font=("Noto Sans KR", -16, "bold")).place(
+        customtkinter.CTkButton(self, text="컬쳐맵", border_width=2, text_font=("Noto Sans KR", -16, "bold"),
+                                command=CultureMapWindow).place(
             relx=0.46, y=7, relwidth=0.175)
         customtkinter.CTkButton(self, text="카테고리", border_width=2, text_font=("Noto Sans KR", -16, "bold")).place(
             relx=0.64, y=7, relwidth=0.175)
@@ -76,6 +84,34 @@ class MenuBar(customtkinter.CTkFrame):
             widget.configure(corner_radius=0)
             widget.configure(fg_color="white")
             widget.configure(border_color="black")
+
+
+class VerticalSlider(customtkinter.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.canvas = customtkinter.CTkCanvas(self)
+        scrollbar = customtkinter.CTkScrollbar(self, orientation="horizontal", command=self.canvas.xview)
+        self.scrollable_frame = customtkinter.CTkFrame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+
+        for i in range(20):
+            global_class = isOpen[i]
+            self.element = HomePageElement(self.scrollable_frame)
+            self.element.grid(row=0, column=i)
+
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        self.canvas.configure(xscrollcommand=scrollbar.set)
+
+        self.canvas.pack(side="top", fill="both", expand=True)
+        scrollbar.pack(side="bottom", fill="x")
 
 
 class HomePageElement(customtkinter.CTkFrame):
@@ -96,21 +132,21 @@ class HomePageElement(customtkinter.CTkFrame):
         self.image.grid(row=0, column=0, padx=10, pady=2)
 
         self.title = customtkinter.CTkLabel(self,
-                                            text="aaaa",
+                                            text=global_class.eventName,
                                             width=200,
                                             height=40,
                                             fg_color="grey")
         self.title.grid(row=1, column=0, padx=10)
 
         self.date = customtkinter.CTkLabel(self,
-                                           text="aaaa",
+                                           text=str(global_class.eventStartDate + " ~ " + global_class.eventEndDate),
                                            width=200,
                                            height=20,
                                            fg_color="grey")
         self.date.grid(row=2, column=0, padx=10)
 
         self.location = customtkinter.CTkLabel(self,
-                                               text="aaaa",
+                                               text=str(global_class.location + global_class.roadAddress),
                                                width=200,
                                                height=20,
                                                fg_color="grey")
@@ -124,57 +160,31 @@ class HomePageElement(customtkinter.CTkFrame):
         self.location.grid(row=3, column=0, padx=10, sticky="e")
 
 
-class VerticalSlider(customtkinter.CTkFrame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.canvas = customtkinter.CTkCanvas(self)
-        scrollbar = customtkinter.CTkScrollbar(self, orientation="horizontal", command=self.canvas.xview)
-        self.scrollable_frame = customtkinter.CTkFrame(self.canvas)
-
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: self.canvas.configure(
-                scrollregion=self.canvas.bbox("all")
-            )
-        )
-
-        for i in range(20):
-            self.element = HomePageElement(self.scrollable_frame)
-            self.element.grid(row=0, column=i)
-
-        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-
-        self.canvas.configure(xscrollcommand=scrollbar.set)
-
-        self.canvas.pack(side="top", fill="both", expand=True)
-        scrollbar.pack(side="bottom", fill="x")
-
-
 class CultureMap(customtkinter.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.map_widget = tkintermapview.TkinterMapView(self, corner_radius=0)
+
+        for cu in allClasses:
+            if hasattr(cu, "eventName"):
+                try:
+                    longitude = float(cu.longitude)
+                    latitude = float(cu.latitude)
+                    self.marker = self.map_widget.set_marker(deg_x=longitude,
+                                                             deg_y=latitude)
+                    self.marker.set_text(cu.eventName)
+                except ValueError:
+                    longitude = float(cu.latitude)
+                    latitude = float(cu.longitude)
+                    self.marker = self.map_widget.set_marker(deg_x=longitude,
+                                                             deg_y=latitude)
+                    self.marker.set_text(cu.eventName)
+
         self.map_widget.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor="center")
 
         self.map_widget.set_position(37.293692, 126.974304)
         self.map_widget.set_zoom(15)
-
-        self.add_markers(isOpen)
-
-    def add_markers(self, p):
-        for c in p:
-            try:
-                marker = self.map_widget.set_marker(deg_x=float(c.latitude),
-                                                    deg_y=float(c.longitude),
-                                                    text=c.eventName,
-                                                    command=CulturePage(c))
-                marker.set_text(c.eventName)
-            except ValueError:
-                pass
-            except AttributeError:
-                pass
 
 
 class CulturePage(customtkinter.CTkToplevel):
